72d4514e7f56f96dfd736db87424a63fcb0748d8
Minji Kim <s2018s24@e-mirim.hs.kr>
2020-04-21 14:19:04 +0900

    버튼 클릭 구현

--- HelloWorldWinform/Form1.Designer.cs
+++ HelloWorldWinform/Form1.Designer.cs
@@ -33,11 +33,12 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("굴림", 30F);
             this.button1.Location = new System.Drawing.Point(42, 25);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(298, 152);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "click";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
